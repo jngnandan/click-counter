e700bfcd0a787b16c68840e7f4f4dddc47b4e1a6
jngnandan <jngnandan@gmail.com>
2021-11-22 10:40:35 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import './App.css';
-import Counter from './components/Counter';
 
+import React from 'react'
+import Counter from './components/Counter'
 
 const App = () => {
   return (
-    <div className="pt-5">
+    <div className="container">
       <Counter />
     </div>
   )
 }
 
-export default App;
+export default App
+
+
+
+
+
+
+
+
+// import './App.css';
+// import Counter from './components/Counter';
+
+// const App = () => {
+//   return (
+//     <div className="pt-5">
+//       <Counter />
+//     </div>
+//   )
+// }
+
+// export default App;
--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,29 +1,91 @@
 
-import { Component } from "react/cjs/react.production.min"
+
+
+
+
+import React, { Component } from 'react'
 
 class Counter extends Component {
-
     state = {
-        currentValue: 0,
+        number: 0
     }
 
     render() {
-        const { currentValue } = this.state
+        const { number } = this.state
 
-        const calculateDigit = () => {
-            this.setState({
-                currentValue: currentValue + 1
-            })
+        const clickTimes = () => {
+            return (
+                this.setState({
+                    number: number + 1
+                })
+            )
         }
 
         return (
-            <div className="container text-center pt-5">
-                <h1 className="text-primary">Button has been clicked <span className="font-weight-bold text-dark"> {currentValue} </span>times</h1>
-                <p>lorem ipsum dolor sit amet, consectetur adipiscing</p>
-                <button className="btn btn-outline-primary" onClick={calculateDigit}>Click here</button>
+            <div className="row vh-100">
+                <div className="align-self-center text-center">
+                    <h1 className="text-dark">The Button has been clicked
+                        <spa className="text-danger">{` ${number}`}</spa> times</h1>
+
+                    <p>Click the button to increase the court!</p>
+                    <button onClick={clickTimes} className="btn btn-primary">Click Me</button>
+                </div>
+
             </div>
         )
     }
-
 }
+
 export default Counter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Component } from "react/cjs/react.production.min"
+
+// class Counter extends Component {
+
+//     state = {
+//         currentValue: 0,
+//     }
+
+//     render() {
+//         const { currentValue } = this.state
+
+//         const calculateDigit = () => {
+//             this.setState({
+//                 currentValue: currentValue + 1
+//             })
+//         }
+
+//         return (
+//             <div className="container text-center pt-5">
+//                 <h1 className="text-primary">Button has been clicked <span className="font-weight-bold text-dark"> {currentValue} </span>times</h1>
+//                 <p>lorem ipsum dolor sit amet, consectetur adipiscing</p>
+//                 <button className="btn btn-outline-primary" onClick={calculateDigit}>Click here</button>
+//             </div>
+//         )
+//     }
+
+// }
+// export default Counter
